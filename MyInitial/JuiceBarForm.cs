68e416118937cc7a6e53c6916c839c7a5580423f
Kyle Javier <kyle.javier@student.tafesa.edu.au>
2023-09-08 22:35:30 +0930

    commit .gitignore file and c# files

--- MyInitial/JuiceBarForm.cs
+++ MyInitial/JuiceBarForm.cs
@@ -163,15 +163,15 @@ namespace MyNameSpace
             switch (selectedSizeRadioButton.Name)
             {
                 case "twelveOunceRadioButton":
-                    itemSizeDecimal = 4m;
+                    itemSizeDecimal = 3m;
                     drinkOrder[itemCountInteger].sizeString = "12 oz";
                     break;
                 case "sixteenOunceRadioButton":
-                    itemSizeDecimal = 5m;
+                    itemSizeDecimal = 3.5m;
                     drinkOrder[itemCountInteger].sizeString = "16 oz";
                     break;
                 case "twentyOunceRadioButton":
-                    itemSizeDecimal = 6m;
+                    itemSizeDecimal = 4m;
                     drinkOrder[itemCountInteger].sizeString = "20 oz";
                     break;
             }
